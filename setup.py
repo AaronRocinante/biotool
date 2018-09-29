ec4358e8ed9c0af3a758c3d9f8784f0f92257e31
Aaron <aaronzhouqian@ucla.edu>
2018-09-28 17:34:16 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup, Extension
 file_binary_search_module = Extension('file_binary_search', sources=['file_search.c'])
 
 setup(name='biotool',
-      version='0.1.7',
+      version='0.1.8',
       license='Apache License, Version 2.0',
-      description='BED file binary search, skips the first line of the file',
+      description='Utilities such as binary search on the lines of a file',
       ext_package='biotool',
       ext_modules=[file_binary_search_module])
